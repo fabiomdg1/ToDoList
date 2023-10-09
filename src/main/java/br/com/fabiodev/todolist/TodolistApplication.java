2aa068d82e32b5c6d6627b276901a80709787dce
Fabio M. Godoy <fabiomdg1@gmail.com>
2023-10-09 09:54:22 -0300

    Testando Controllers

--- src/main/java/br/com/fabiodev/todolist/TodolistApplication.java
+++ src/main/java/br/com/fabiodev/todolist/TodolistApplication.java
@@ -3,7 +3,7 @@ package br.com.fabiodev.todolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //Define que a classe abaixo é a clase inicial do projeto
 public class TodolistApplication {
 
 	public static void main(String[] args) {
