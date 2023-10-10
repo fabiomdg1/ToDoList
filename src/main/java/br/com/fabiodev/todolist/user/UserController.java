618131b3649e2e791aee6a17299095ed0b8784b8
Fabio M. Godoy <fabiomdg1@gmail.com>
2023-10-10 09:31:04 -0300

    Conexao com banco de dados H2 para testes

--- src/main/java/br/com/fabiodev/todolist/user/UserController.java
+++ src/main/java/br/com/fabiodev/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.fabiodev.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
  
-    @PostMapping
-    public void create(@RequestBody UserModel usermodel){
-        System.out.println(usermodel.name);
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel usermodel){
+        var user = this.userRepository.findByUsername(usermodel.getUsername());
+
+        if(user != null){
+            System.out.println("Já existe este usuário cadastrado.");
+            // Mensagem de erro
+            // Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        var userCreated = userRepository.save(usermodel);
+        System.out.println(userCreated);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+
     }
     
     @RequestMapping("/teste")
