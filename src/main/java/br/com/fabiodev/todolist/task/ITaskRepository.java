f10dfb25a4ed670ccab763434389e5fa78e895ee
Fabio M. Godoy <fabiomdg1@gmail.com>
2023-10-13 17:16:53 -0300

    Corrigido erro de interface

--- src/main/java/br/com/fabiodev/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/fabiodev/todolist/task/ITaskRepository.java
@@ -1,9 +1,11 @@
 package br.com.fabiodev.todolist.task;
 
 import java.util.UUID;
-
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
-    
+    //List<TaskModel> findyByIdUser(UUID idUser);
+    List<TaskModel> findByUserId(UUID userId);
+
 }
--- src/main/java/br/com/fabiodev/todolist/task/TaskController.java
+++ src/main/java/br/com/fabiodev/todolist/task/TaskController.java
@@ -2,11 +2,13 @@ package br.com.fabiodev.todolist.task;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.experimental.var;
 
 @RestController
 @RequestMapping("/tasks")
@@ -27,7 +30,7 @@ public class TaskController {
         
         var idUser = request.getAttribute("idUser");
 
-        taskModel.setIdUser((UUID) idUser);
+        taskModel.setUserId((UUID) idUser);
 
         var currentDate = LocalDateTime.now();
 
@@ -43,6 +46,13 @@ public class TaskController {
         var task = this.taskRepository.save(taskModel);
         System.out.println("task: " + task);
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByUserId((UUID) idUser);
+        return tasks;
     }
     
 }
